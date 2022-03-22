5f762a56e875e34d95744dbd0ce05c3ce2632b19
MyCatStoleMyCode <8713509+MyCatStoleMyCode@users.noreply.github.com>
2022-03-22 16:37:17 -0700

    Added parse secrets to gitignore and config

--- weather-app/AppDelegate.swift
+++ weather-app/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = Secrets.applicationId
+            $0.clientKey = Secrets.clientKey
+            $0.server = Secrets.server
+        }
+        
         return true
     }
 
